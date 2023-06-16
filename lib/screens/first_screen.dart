a0821b57d3843f9199b2d68761f32ad700aeaf6b
이진실 <79292184+ljsil1204@users.noreply.github.com>
2023-06-16 12:21:45 +0900

    카운터 예제 - UI, 상태 정의

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,4 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_example_project/screens/second_screen.dart';
 
-class FirstScreen extends StatelessWidget {}
+class FirstScreen extends StatefulWidget {
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter App"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "count : ",
+              style: TextStyle(fontSize: 20),
+            ),
+            Padding(padding: EdgeInsets.all(20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(onPressed: () {}, child: Text("-down")),
+                ElevatedButton(onPressed: () {}, child: Text("+up"))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
