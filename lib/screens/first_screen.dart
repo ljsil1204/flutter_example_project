2d5911402b0f1597aae1aa9ef6cbf1f13671317c
이진실 <79292184+ljsil1204@users.noreply.github.com>
2023-06-16 12:15:15 +0900

    Update first_screen.dart

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,27 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_example_project/screens/second_screen.dart';
 
-class FirstScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("first screen"),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (BuildContext context) => SecondScreen(
-                            screenData: "data from first screen",
-                          )));
-                },
-                child: Text("go second"))
-          ],
-        ),
-      ),
-    );
-  }
-}
+class FirstScreen extends StatelessWidget {}
